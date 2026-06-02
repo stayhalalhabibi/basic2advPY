68d3507ba5a91edb09f648f3cab82894b706130e
Sharif <stayhalalhabibi002@gmail.com>
2026-06-02 11:31:24 +0530

    Add a file

---
+++ 01_basic.py
@@ -0,0 +1,52 @@
+print("salam sharif")
+
+str1 = "sam"
+print(str1)         # sam
+print(type(str1))   # str
+
+a = "10"            
+print(a)            # 10
+print(type(a))      # str
+
+b = 20              
+print(type(b))      # int
+
+s = True
+print(type(s))      # boolean
+
+v = False
+print(type(v))      # boolean
+
+n = "False"
+print(n)            # False
+print(type(n))      #str
+
+print(a,b,s,v,n,)   # 10 20 True False False
+
+name = "sharifur rahman"
+age = 20
+marks = 82.876
+print(name , "is", age , "years old and got the", marks ,
+      "marks")                                                                # sharifur rahman is 20 years old and got the 82.876 marks
+print("%s is %d years old and got the %f marks" %(name,age,marks))            # sharifur rahman is 20 years old and got the 82.876 marks
+
+print(type(name))    # str
+print(type(age))     # int
+print(type(marks))   # float
+
+print(f"{name} is {age} year old and got the {marks} marks")                 # sharifur rahman is 20 years old and got the 82.876 marks
+
+print("%s is %d years old and got the %2f marks" %(name,age,marks))            # sharifur rahman is 20 years old and got the 82.876 marks
+
+str1 = "sharif"
+print(type(str1))          # string
+name = " rahman"
+print(str1 + name)         # sharifur rahman
+
+k = 12
+l = 34
+print(k>l)                 # False
+print(k==l)                 # False
+print(k<l)                 # True
+
+
